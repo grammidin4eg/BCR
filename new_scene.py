1012603c5e64e9942addbe81242b3e8b4a965cbb
grammidin4eg <grammidin4eg@gmail.com>
2022-11-03 16:53:11 +0300

    new scene

---
+++ new_scene.py
@@ -0,0 +1,11 @@
+import pygame
+from StageManagement import Stage
+
+
+class NewGameScene(Stage):
+    def __init__(self):
+        super().__init__()
+    def render(self, screen):
+        pass
+    def update(self, events):
+        pass
